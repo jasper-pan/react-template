12406d6d44d82b61758392fac181f545535789da
jialin.pan <jialin.pan@openjawtech.com>
2019-12-13 17:47:11 +0800

    add ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+interface AppType {
+  name:string
+}
+const App = function (props:AppType) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.{props.name}
         </p>
         <a
           className="App-link"
